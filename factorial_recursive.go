db9d36cc8cee3f757540c5d6991b10547753b723
Saurabhkanawade <saurabhkanawade30@gmail.com>
2025-01-14 19:39:16 +0530

    added recursive function of factorial

--- factorial_recursive.go
+++ factorial_recursive.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func Factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * Factorial(n-1)
+}
+
 func main() {
-	fmt.Println("find the factorial of the no")
+	var number int
+	_, err := fmt.Scan(&number)
+	if err != nil {
+		return
+	}
+	fmt.Printf("The factorial of the %d is : %d", number, Factorial(number))
 }
