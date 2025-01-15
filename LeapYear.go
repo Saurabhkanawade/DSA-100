ab6217453094a51cc48a01cf1ed54377a4665495
Saurabh Kanawade <saurabhkanawade30@gmail.com>
2025-01-15 20:24:04 +0530

    formated

--- LeapYear.go
+++ LeapYear.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//LeapYear return formated string
 func LeapYear(year int) string {
 	if year%4 == 0 || year%400 == 0 && year%100 != 0 {
 		return fmt.Sprintf("The given year %d is Leap Year", year)
