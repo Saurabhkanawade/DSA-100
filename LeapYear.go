37a84e987a48288dbd8ec0b03223810a136fa1b5
Saurabh Kanawade <saurabhkanawade30@gmail.com>
2025-01-15 20:22:20 +0530

    solution for leap year

---
+++ LeapYear.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func LeapYear(year int) string {
+	if year%4 == 0 || year%400 == 0 && year%100 != 0 {
+		return fmt.Sprintf("The given year %d is Leap Year", year)
+	} else {
+		return fmt.Sprintf("The given year %d is NOT Leap Year", year)
+	}
+}
+
+func main() {
+	fmt.Println(LeapYear(2048))
+}
