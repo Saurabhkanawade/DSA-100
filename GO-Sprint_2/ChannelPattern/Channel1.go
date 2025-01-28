33576ab239c021b2cb5a0679435751de56d59033
Saurabh Kanawade <saurabhkanawade30@gmail.com>
2025-01-28 10:54:38 +0530

    updated

--- GO-Sprint_2/ChannelPattern/Channel1.go
+++ GO-Sprint_2/ChannelPattern/Channel1.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
+//channel without the waitgroup
 func main() {
 
-	myChannel := make(chan string)
+	myChannel := make(chan string, 1)
 
 	go func() {
 		defer close(myChannel)
-
 		fmt.Println("putting the data in channel")
 		myChannel <- "data"
-
+		myChannel <- "data"
 	}()
 
 	channelData := <-myChannel
 
+	fmt.Println(channelData)
 	fmt.Println(channelData)
 	fmt.Println("main function")
 }
